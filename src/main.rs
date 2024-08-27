601f4c19204531cccddbc2b2ab4a3de5def25ced
Lynn <emmalynnchenn@gmail.com>
2024-08-27 16:16:05 -0500

    attempting to create a button that opens directory on local machine

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,51 @@
 use yew::prelude::*;
 
-struct Model {
-    value: i64,
+pub struct Button {
+    label: String,
+    onclick: Callback<()>,
+}
+
+#[derive(Properties, PartialEq)]
+pub struct ButtonProps {
+    pub label: String,
+    pub onclick: Callback<()>,
+}
+
+impl Component for Button {
+    type Message = ();
+    type Properties = ButtonProps;
+
+    fn create(ctx: &Context<Self>) -> Self {
+        Self {
+            label: ctx.props().label.clone(),
+            onclick: ctx.props().onclick.clone(),
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
+        false
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let onclick = ctx.link().callback(|_| {
+            self.onclick.emit(());
+        });
+
+        html! {
+            <button {onclick}>{ &self.label }</button>
+        }
+    }
 }
 
 #[function_component(App)]
 fn app() -> Html {
-    let state = use_state(|| Model { value: 0 });
-
-    let onclick = {
-        let state = state.clone();
-
-        Callback::from(move |_| {
-            state.set(Model {
-                value: state.value + 1,
-            })
-        })
-    };
+    let open_directory = Callback::from(|| {
+        //open directory
+    });
 
     html! {
-        <div>
-            <button {onclick}>
-                { "+1" }
-            </button>
-            <p>{ state.value }</p>
-        </div>
+        <Button label="Add" {open_directory} />
     }
 }
 
-fn main() {
-    yew::start_app::<App>();
-}
+fn main() {}
